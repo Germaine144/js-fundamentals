995fda34ac2eab7fc641273241cf23c47c2122bb
Germaine144 <umuhiregermaine12@gmail.com>
2025-03-31 16:22:23 +0200

    fnct of snake-casw

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function isPalindrome(str) {
     return str === reverseString(str);
 }
 console.log(isPalindrome('racecars')); 
+// Write a function that counts the number of words in a given string.
+function  wordCoun(str) {
+    return str.trim().split(/\s+/).length;
+}
+console.log( wordCoun('hello world')); 
+//Write a function that converts a string to snake_case
+function toSnakeCase(str) {
+    return str.replace(/[^A-Za-z0-9]/g, '_').toLowerCase();
+}
+console.log(toSnakeCase('Hello Classification')); 
